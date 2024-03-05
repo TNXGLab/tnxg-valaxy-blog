519a3f2671d69707efe35f1467db8df1304f1a78
TNXG <tnxg@outlook.jp>
2024-03-05 21:21:42 +0800

    first commit

---
+++ demo/valaxy.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig } from 'valaxy'
+import type { ThemeConfig } from 'valaxy-theme-keshiki'
+
+/**
+ * User Config
+ * do not use export const config to avoid defu conflict
+ */
+export default defineConfig<ThemeConfig>({
+  theme: 'valaxy-theme-keshiki',
+
+  themeConfig: {
+    // colors: {
+    //   primary: 'red',
+    // },
+
+    nav: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/YunYouJun/valaxy-theme-starter',
+      },
+      {
+        text: 'RSS',
+        link: '/atom.xml',
+      },
+      {
+        text: 'Discord',
+        link: 'https://discord.gg/sGe4U4p4CK',
+      },
+      {
+        text: 'Valaxy →',
+        link: 'https://github.com/YunYouJun/valaxy',
+      },
+    ],
+
+    footer: {
+      since: 2016,
+    },
+  },
+})
